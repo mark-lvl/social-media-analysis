a5ccea59072afdccfac88b839f0329fae3608844
Mark <markteams@gmail.com>
2018-07-11 23:05:47 +0300

    refactoring the code structure

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sys
-import src.twitter_client as api_client
+import src.twitter_client as tw
 import src.sentiment as sentiment
 import argparse
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(prog='main',
                                      usage='%(prog)s func [options]',
                                      description='Tweets analysis')
@@ -18,31 +17,35 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    twitter = tw.Twitter()
+
     if args.func == 'sentiment':
         # input for term to be searched and how many tweets to search
-        phrase = input("Enter Keyword/Tag to search about: ")
-        phrase_count = int(input("Enter how many tweets to search: "))
+        term = input("Enter Keyword/Tag to search about: ")
+        num_tweets = int(input("Enter how many tweets to search: "))
+        read_from_cache = bool(int(input("Reading from cache?")))
 
-        tweets, percentages = sentiment.sentiment_analysis(api_client.get_tweets(phrase,
-                                                                                 phrase_count,
-                                                                                 cache=True,
-                                                                                 fetch_new=False))
+        tweets, percentages = sentiment.sentiment_analysis(twitter.search_tweets(term,
+                                                                                 num_tweets,
+                                                                                 export=True,
+                                                                                 cache_only=read_from_cache))
 
         # print percentages in console and draw graph
-        sentiment.sentiment_report(percentages, graph=True, phrase=phrase, phrase_count=len(tweets))
+        sentiment.sentiment_report(percentages, graph=True, phrase=term, phrase_count=len(tweets))
 
-    if args.func == 'user':
-        # input for term to be searched and how many tweets to search
-        user = input("Enter user name to search about: ")
-        phrase_count = int(input("Enter how many tweets to search: "))
-        fetch_new = int(input("Fetching new tweets and append to cache?"))
-
-        tweets = api_client.get_user_tweets(user, phrase_count, cache=True, fetch_new=fetch_new)
-
-        print(len(tweets))
-        # for tweet_id, tweet in tweets.items():
-        #     print(tweet_id, ":", tweet['text'])
-
-    if args.func == 'stream':
-        phrase = input("Enter Keyword/Tag to live search about: ")
-        api_client.get_stream(phrase)
+    # if args.func == 'user':
+    #     # input for term to be searched and how many tweets to search
+    #     user = input("Enter user name to search about: ")
+    #     phrase_count = int(input("Enter how many tweets to search: "))
+    #     fetch_new = int(input("Fetching new tweets and append to cache?"))
+    #
+    #     tweets = api_client.get_user_tweets(user, phrase_count, cache=True, fetch_new=fetch_new)
+    #
+    #     print(len(tweets))
+    #     # for tweet_id, tweet in tweets.items():
+    #     #     print(tweet_id, ":", tweet['text'])
+    #
+    # if args.func == 'stream':
+    #     phrase = input("Enter Keyword/Tag to live search about: ")
+    #     phrase_count = int(input("Enter how many tweets to receive: "))
+    #     api_client.get_stream(phrase, phrase_count)
--- src/twitter_client.py
+++ src/twitter_client.py
@@ -1,122 +1,196 @@
-from src import config
+import configparser
 from src import helpers as h
-import sys
+import os
 from tweepy import API
 from tweepy import OAuthHandler
 from tweepy import Cursor
-import tweepy
-from tweepy.streaming import StreamListener
-from tweepy import Stream
-import time
-import json
+# import tweepy
+# from tweepy.streaming import StreamListener
+# from tweepy import Stream
+# import time
+# import json
 
 
-def get_twitter_auth():
-    """Setup Twitter authentication.
-    Return: tweepy.OAuthHandler object
-    """
-    try:
-        consumer_key = config.TWITTER_CONFIG['consumer_key']
-        consumer_secret = config.TWITTER_CONFIG['consumer_secret']
-        access_token = config.TWITTER_CONFIG['access_token_key']
-        access_secret = config.TWITTER_CONFIG['access_token_secret']
-    except KeyError:
-        sys.stderr.write("TWITTER_* environment variables not set\n")
-        sys.exit(1)
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
-    return auth
+class Twitter(object):
+
+    def __init__(self):
+        config = configparser.ConfigParser()
+
+        config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini'))
+
+        if 'twitter.api' not in config.sections():
+            raise ValueError("Config file not found.")
+
+        config_ini = config['twitter.api']
+
+        self.auth = OAuthHandler(config_ini['ConsumerKey'], config_ini['ConsumerSecret'])
+        self.auth.set_access_token(config_ini['AccessTokenKey'], config_ini['AccessTokenSecret'])
+
+        self.api = API(self.auth)
+
+    def search_tweets(self, term, num_tweets, export=False, cache_only=True):
+        # looking for cache
+        cached_tweets = h.load_output(term, path='output/tweets')
+
+        if cached_tweets and cache_only:
+            return cached_tweets
+
+        # query term to fetch tweets
+        query_results = Cursor(self.api.search, q=term, lang="en").items(num_tweets)
+
+        # construct tweets dictionary
+        tweets = {tweet.id: tweet._json for tweet in query_results}
+
+        # merge cached and recent tweets if there are any
+        if cached_tweets:
+            tweets = {**tweets, **cached_tweets}
+
+        # casting key to int in order to silent sorting error
+        tweets = {int(k): v for k, v in tweets.items()}
+
+        if export:
+            h.dump_output(tweets, term, path='output/tweets')
+
+        return tweets
+
+# def get_twitter_auth():
+#     """Setup Twitter authentication.
+#     Return: tweepy.OAuthHandler object
+#     """
+#     try:
+#         consumer_key = config.TWITTER_CONFIG['consumer_key']
+#         consumer_secret = config.TWITTER_CONFIG['consumer_secret']
+#         access_token = config.TWITTER_CONFIG['access_token_key']
+#         access_secret = config.TWITTER_CONFIG['access_token_secret']
+#     except KeyError:
+#         sys.stderr.write("TWITTER_* environment variables not set\n")
+#         sys.exit(1)
+#     auth = OAuthHandler(consumer_key, consumer_secret)
+#     auth.set_access_token(access_token, access_secret)
+#     return auth
+
+# def get_twitter_auth():
+#     """Setup Twitter authentication.
+#     Return: tweepy.OAuthHandler object
+#     """
+#     try:
+#         config_file = configparser.ConfigParser()
+#         config_file.read('./config.ini')
+#         config = config_file['DEFAULT']
+#     except KeyError:
+#         sys.stderr.write("TWITTER_* environment variables not set\n")
+#         sys.exit(1)
+#     auth = OAuthHandler(config['ConsumerKey'], config['ConsumerSecret'])
+#     auth.set_access_token(config['AccessTokenKey'], config['AccessTokenSecret'])
+#     return auth
+#
+# def get_twitter_client():
+#     """Setup Twitter API client.
+#     Return: tweepy.API object
+#     """
+#     auth = get_twitter_auth()
+#     client = API(auth)
+#     return client
 
 
-def get_twitter_client():
-    """Setup Twitter API client.
-    Return: tweepy.API object
-    """
-    auth = get_twitter_auth()
-    client = API(auth)
-    return client
+# def get_twitter_client():
+#     """Setup Twitter API client.
+#     Return: tweepy.API object
+#     """
+#     auth = get_twitter_auth()
+#     client = API(auth)
+#     return client
 
 
-def limit_handled(cursor):
-    while True:
-        try:
-            yield cursor.next()
-        except tweepy.RateLimitError:
-            time.sleep(15 * 60)
+# def limit_handled(cursor):
+#     while True:
+#         try:
+#             yield cursor.next()
+#         except tweepy.RateLimitError:
+#             time.sleep(15 * 60)
 
 
-def get_tweets(phrase, tweets_count, cache=False, fetch_new=False):
-    # looking for cache
-    cached_tweets = h.load_output(phrase, path='output/tweets')
-
-    if cached_tweets and not fetch_new:
-        return cached_tweets
-
-    # query phrase to fetch tweets
-    query_results = Cursor(get_twitter_client().search, q=phrase, lang="en").items(tweets_count)
-
-    # construct tweets dictionary
-    tweets = {tweet.id:tweet._json for tweet in query_results}
-
-    # merge cached and recent tweets if there are any
-    if cached_tweets:
-        tweets = {**tweets, **cached_tweets}
-
-    # casting key to int in order to silent sorting error
-    tweets = {int(k): v for k, v in tweets.items()}
-
-    if cache:
-        h.dump_output(tweets, phrase, path='output/tweets')
-
-    return tweets
-
-
-def get_user_tweets(user, tweets_count, cache=False, fetch_new=False):
-    # looking for cache
-    cached_tweets = h.load_output(user, path='output/user')
-
-    if cached_tweets and not fetch_new:
-        return cached_tweets
-
-    page_count = 1
-    if tweets_count > 200:
-        page_count = int(tweets_count / 200) + 1
-        tweets_count = 200
-
-    # query phrase to fetch tweets
-    query_results = Cursor(get_twitter_client().user_timeline, screen_name=user, count=tweets_count).pages(page_count)
-
-    # construct tweets dictionary
-    tweets = {tweet.id:tweet._json for page in query_results for tweet in page}
-
-    # merge cached and recent tweets if there are any
-    if cached_tweets:
-        tweets = {**tweets, **cached_tweets}
-
-    # casting key to int in order to silent sorting error
-    tweets = {int(k): v for k, v in tweets.items()}
-
-    if cache:
-        h.dump_output(tweets, user, path='output/user')
-
-    return tweets
+# def get_tweets(phrase, num_tweets, cache=False, fetch_new=False):
+#     # looking for cache
+#     cached_tweets = h.load_output(phrase, path='output/tweets')
+#
+#     if cached_tweets and not fetch_new:
+#         return cached_tweets
+#
+#     # query phrase to fetch tweets
+#     query_results = Cursor(get_twitter_client().search, q=phrase, lang="en").items(num_tweets)
+#
+#     # construct tweets dictionary
+#     tweets = {tweet.id: tweet._json for tweet in query_results}
+#
+#     # merge cached and recent tweets if there are any
+#     if cached_tweets:
+#         tweets = {**tweets, **cached_tweets}
+#
+#     # casting key to int in order to silent sorting error
+#     tweets = {int(k): v for k, v in tweets.items()}
+#
+#     if cache:
+#         h.dump_output(tweets, phrase, path='output/tweets')
+#
+#     return tweets
 
 
-class TwitterListener(StreamListener):
-
-    def on_data(self, data):
-        j = json.loads(data)
-        print(j["text"])
-        return True
-
-    def on_error(self, status):
-        print(status)
-
-
-def get_stream(phrase):
-    twitter_stream = Stream(get_twitter_auth(), TwitterListener())
-    try:
-        twitter_stream.filter(track=[phrase], async=True)
-    except Exception as e:
-        print(e.__doc__)
-
+# def get_user_tweets(user, num_tweets, cache=False, fetch_new=False):
+#     # looking for cache
+#     cached_tweets = h.load_output(user, path='output/user')
+#
+#     if cached_tweets and not fetch_new:
+#         return cached_tweets
+#
+#     page_count = 1
+#     if num_tweets > 200:
+#         page_count = int(num_tweets / 200) + 1
+#         num_tweets = 200
+#
+#     # query phrase to fetch tweets
+#     query_results = Cursor(get_twitter_client().user_timeline, screen_name=user, count=num_tweets).pages(page_count)
+#
+#     # construct tweets dictionary
+#     tweets = {tweet.id: tweet._json for page in query_results for tweet in page}
+#
+#     # merge cached and recent tweets if there are any
+#     if cached_tweets:
+#         tweets = {**tweets, **cached_tweets}
+#
+#     # casting key to int in order to silent sorting error
+#     tweets = {int(k): v for k, v in tweets.items()}
+#
+#     if cache:
+#         h.dump_output(tweets, user, path='output/user')
+#
+#     return tweets
+#
+#
+# class TwitterListener(StreamListener):
+#     def __init__(self, num_tweets=100):
+#         self.counter = 0
+#         self.num_tweets = num_tweets
+#
+#     def on_data(self, data):
+#         try:
+#             j = json.loads(data)
+#             print('='*40)
+#             print(j["text"])
+#             self.counter += 1
+#             if self.counter == self.num_tweets:
+#                 return False
+#             return True
+#         except:
+#             pass
+#
+#     def on_error(self, status):
+#         print(status)
+#
+#
+# def get_stream(phrase, phrase_count=100):
+#     twitter_stream = Stream(get_twitter_auth(), TwitterListener(num_tweets=phrase_count))
+#     try:
+#         twitter_stream.filter(track=[phrase], async=True)
+#     except Exception as e:
+#         print(e.__doc__)
